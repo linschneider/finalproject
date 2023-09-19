fc3e2615f43718e7e5e2125133b4053f53f39703
linschneider <136378548+linschneider@users.noreply.github.com>
2023-09-19 13:21:45 +0300

    Update app_backend.py

--- app_backend.py
+++ app_backend.py
@@ -1,23 +1,33 @@
 from flask import Flask, render_template
-from pymongo import MongoClient
+import pymongo
 
 app = Flask(__name__)
 
+# Step 2: Establish a connection to MongoDB
+client = pymongo.MongoClient("mongodb://localhost:27017/")  # Change the connection string as needed
+
+# Step 3: Access the collection
+db = client["finalprojectapp"]  # Replace with your database name
+collection = db["dbapp"]  # Replace with your collection name
+
+# Step 4: Collect data from the collection
+# For example, let's retrieve all documents from the collection
+documents = collection.find()
+
+# Create a list to store the documents
+document_list = []
+
+# Iterate over the documents and append them to the list
+for document in documents:
+    document_list.append(document)
+
+# Close the MongoDB connection
+client.close()
 
 @app.route('/')
 def index():
-    try:
-        # Connect to the MongoDB database and collection
-        client = MongoClient('mongodb://localhost:27017')
-        db = client.finalprojectapp
-        collection = db.dbapp
-        status = "Connected to MongoDB"
-        # client.close()
-    except Exception as e:
-        status = f"Failed to connect to MongoDB: {str(e)}"
-
-    return render_template('app_front.html', status=status)
-
+    print(document_list)
+    return render_template('app_front.html', collection=document_list)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", debug=True)
